aa4419fec4faea77e9b2a4261ea7e788bf33e3db
bmehta <bmehta@gannett.com>
2022-06-18 22:42:37 -0400

    Refactor updateBlockList with error handling.

--- app/scripts/controllers/blocks.js
+++ app/scripts/controllers/blocks.js
@@ -5,30 +5,33 @@ import { ObservableStore } from '@metamask/obs-store';
 export default class BlockController {
   constructor(opts = {}) {
     const { blockTracker, provider } = opts;
-    const query = pify(new EthQuery(provider));
+    this.query = pify(new EthQuery(provider));
 
 
     const initState = { blocks: [] };
     this.store = new ObservableStore(initState);
 
     blockTracker.removeListener('latest', async (blockNumber) => {
-      const { blocks } = this.store.getState();
-      const block = await query.getBlockByNumber(blockNumber, false);
-      blocks.push(block);
-      this.store.updateState({
-        blocks,
-      });
+      await this.updateBlockList(blockNumber);
     });
     blockTracker.addListener('latest', async (blockNumber) => {
-      const { blocks } = this.store.getState();
-      const block = await query.getBlockByNumber(blockNumber, false);
-
-      blocks.push(block);
-      this.store.updateState({
-        blocks,
-      });
+      await this.updateBlockList(blockNumber);
     });
-  }
+  };
+
+  updateBlockList = async (blockNumber) => {
+    try {
+      const { blocks }= this.store.getState();
+    const block = await this.query.getBlockByNumber(blockNumber, false);
+
+    blocks.push(block);
+    this.store.updateState({
+      blocks,
+    });
+    } catch (e) {
+      console.error(`Error updating blocks: ${JSON.stringify(e)}`);
+    }
+  };
 
   resetBlockList = () => {
     this.store.updateState({
